c668033ac07047d8abb55cdcae6d5d26ec955cb3
benjaglobant <benjaglobant@users.noreply.github.com>
2020-07-14 15:34:33 -0300

    service to get houses id added
    code refactor
    
    house table database added
    
    adding comment
    
    comments fixed
    
    code refactor
    
    parent d34f80223c781423982bdb07d0c6a14de288b964
    author benjaglobant <benjaglobant@users.noreply.github.com> 1594839645 -0300
    committer benjaglobant <benjaglobant@users.noreply.github.com> 1595349488 -0300
    
    parent d34f80223c781423982bdb07d0c6a14de288b964
    author benjaglobant <benjaglobant@users.noreply.github.com> 1594839645 -0300
    committer benjaglobant <benjaglobant@users.noreply.github.com> 1595349252 -0300
    
    house UI added
    
    comments fixed
    
    house detail ui added
    
    UI refactor
    
    house detail service added
    
    adding and refactoring tests
    
    comments fixed
    
    house detail database added
    
    house detail database tests added
    
    comments fixed
    
    UI refactor
    
    house detail database tests added
    
    comments fixed

--- app/src/main/java/com/globant/harrypotterapp/util/Data.kt
+++
@@ -1,9 +0,0 @@
-package com.globant.harrypotterapp.util
-
-data class Data<RequestData>(var status: Status, var data: RequestData? = null, var error: Exception? = null)
-
-enum class Status {
-    LOADING,
-    SUCCESS,
-    ERROR
-}
--- domain/src/main/java/com/globant/domain/database/HarryPotterRoomDataBase.kt
+++
@@ -1,9 +0,0 @@
-package com.globant.domain.database
-
-import com.globant.domain.entity.Spell
-import com.globant.domain.util.Result
-
-interface HarryPotterRoomDataBase {
-    fun getSpellsFromDataBase(): Result<List<Spell>>
-    fun updateSpells(spells: List<Spell>)
-}
